cfb93118a5372b0365646679177abe8d095b442a
OyamaErika <oyama.erika@tis.co.jp>
2018-06-13 20:48:12 +0900

    新規作成

--- src/main/java/codecheck/App.java
+++ src/main/java/codecheck/App.java
@@ -1,10 +1,48 @@
 package codecheck;
 
+import java.util.ArrayList;
+
 public class App {
 	public static void main(String[] args) {
-		for (int i = 0, l = args.length; i < l; i++) {
-			String output = String.format("argv[%s]: %s", i, args[i]);
-			System.out.println(output);
+		int crd = Integer.parseInt(args[0]);
+		ArrayList<Integer> listPower = new ArrayList<Integer>();
+		ArrayList<Integer> listCost = new ArrayList<Integer>();
+		int num = 0;
+
+		for (int i = 0 ; num < crd; i = i+2) {
+			listPower.add(num, Integer.parseInt(args[i+2]));
+			listCost.add(num, Integer.parseInt(args[i+3]));
+			num++;
 		}
+
+		int mp = Integer.parseInt(args[1]);
+		int ans = 0;
+		int deleteFlg = 1;
+
+		while (deleteFlg == 1) {
+			int max = 0;
+			int power = 0;
+			int cost = 0;
+			int deleteNum = 0;
+
+			for (int i = 0, l = listPower.size(); i < l; i++) {
+				power = listPower.get(i);
+				cost = listCost.get(i);
+				if (power > max && mp >= cost) {
+					max = power;
+					deleteNum = i;
+				}
+			}
+
+			if (max == 0) {
+				deleteFlg = 0;
+			} else {
+				mp = mp - listCost.get(deleteNum);
+				ans = ans + max;
+				listPower.remove(deleteNum);
+				listCost.remove(deleteNum);
+			}
+		}
+		System.out.println(ans);
 	}
 }
